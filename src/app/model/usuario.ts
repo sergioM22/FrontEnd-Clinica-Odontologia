aec78f22b5dd6095c761866d23ff4dcdf981e7de
sergioM22 <sergio.2435@hotmail.com>
2023-11-05 13:33:08 -0500

    Registro/eliminacion/listado de citas corregido (editar pendiente)

--- src/app/model/usuario.ts
+++ src/app/model/usuario.ts
@@ -1,9 +1,10 @@
 export class Usuario{
-    idUsuario?: number;
-    username?: string;
-    password?: string;
-    nombre?: string;
-    apellido?: string;
-    email?: string;
-    telefono?: string;
+    idUsuario: number;
+    username: string;
+    password: string;
+    nombre: string;
+    apellido: string;
+    email: string;
+    telefono: string;
+    perfil: string;
 }
